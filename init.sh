843ed30e25c60e0bcbaefcb476618eb0be1a1dbd
Theprotocolwhisperer <chrismata10@gmail.com>
2023-08-26 17:18:27 -0500

    (feature)  :  Bash start

---
+++ init.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+cat << "EOF"
+·▄▄▄▄  ▄▄▄ .▄▄▄  ▄▄▄ .▄▄▌ ▐ ▄▌ ▄▄▄· ▄▄▄  ·▄▄▄▄  
+██▪ ██ ▀▄.▀·▀▄ █·▀▄.▀·██· █▌▐█▐█ ▀█ ▀▄ █·██▪ ██ 
+▐█· ▐█▌▐▀▀▪▄▐▀▀▄ ▐▀▀▪▄██▪▐█▐▐▌▄█▀▀█ ▐▀▀▄ ▐█· ▐█▌
+██. ██ ▐█▄▄▌▐█•█▌▐█▄▄▌▐█▌██▐█▌▐█ ▪▐▌▐█•█▌██. ██ 
+▀▀▀▀▀•  ▀▀▀ .▀  ▀ ▀▀▀  ▀▀▀▀ ▀▪ ▀  ▀ .▀  ▀▀▀▀▀▀• 
+EOF
+
+# ... rest of your script here
+sleep 2  # You can adjust the sleep time if you want it to pause longer
+
+cat << "EOF"
+
+   .    .    .   Loading...   .    .    .
+
+EOF
+
+# Execute docker-compose up
+docker-compose up
+#Run worker that consumes Events logs from Oracle Consumer
+npm run worker || yarn run worker || pnpm run worker
